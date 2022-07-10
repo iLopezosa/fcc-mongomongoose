4e2190c0eafe6a414701a359e68ede3da4b4c38c
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-07-10 22:20:57 +0200

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://n4xo:980822@fcc-mongo-ilopezosa.zmzkq0a.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
